a73e0c5b65a3ae818e6decd485b7645a78affa99
Coridyn Fitzgerald-Hood <coridyn@gmail.com>
2021-02-17 18:59:42 +1000

    chore(format): add .editorconfig; reformat source

--- bin/defaults.js
+++ bin/defaults.js
@@ -9,13 +9,12 @@ parser.addArgument('schema', {help: 'JSON Schema file'});
 
 var args = parser.parseArgs();
 
-fs.readFile(args.schema, 'utf-8', function(err, data) {
-  if (err)
-    return console.error('Could not read file "' + args.schema + '"');
-  try {
-    console.log(JSON.stringify(defaults(JSON.parse(data)), null, args.indent));
-  }
-  catch (e) {
-    console.error('Invalid JSON Schema');
-  }
+fs.readFile(args.schema, 'utf-8', function (err, data) {
+    if (err)
+        return console.error('Could not read file "' + args.schema + '"');
+    try {
+        console.log(JSON.stringify(defaults(JSON.parse(data)), null, args.indent));
+    } catch (e) {
+        console.error('Invalid JSON Schema');
+    }
 });
